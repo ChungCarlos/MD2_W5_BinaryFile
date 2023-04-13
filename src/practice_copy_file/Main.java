9f243e79f20d465566509b36a69fa876743337c5
Chung <Joschungcarlos95@gmail.com>
2023-04-13 16:42:57 +0700

    done practice read write dssv

--- src/practice_copy_file/Main.java
+++ src/practice_copy_file/Main.java
@@ -28,7 +28,7 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         Scanner in = new Scanner(System.in);
 
         System.out.printf("Enter source file:");
